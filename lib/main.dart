4174b19500a58466404a0b45ffe17682856f28c2
marioutomousm <marioutomo.usm@gmail.com>
2023-12-12 10:19:10 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutterlist/userdata.dart';
+import 'package:flutterlist/useritem.dart';
+import 'package:flutterlist/userlist.dart';
+
+void main() {
+  // UserData userIdris = UserData("Idris", 34, "idrez.mochamad@gmail.com");
+  runApp(UserList());
+}
